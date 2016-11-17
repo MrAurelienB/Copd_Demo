6028dfc2cd118b4e3711a066faf235fa14335804
MrAurelienB <aurelien.bach@hotmail.fr>
2016-11-17 15:36:47 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -11,14 +11,33 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$contents <- renderTable({
-    inFile <- input$file_data
+  
+  data <- data.frame(0,0)
+  
+  output$data_contents <- renderUI({
+    inFile <- input$data_file
     if (is.null(inFile))
       return(NULL)
-    read.csv(inFile$datapath, header = input$header, sep = ",")
+    
+    data <- read.csv(inFile$datapath, header = input$header, sep = input$sep)
+    choices <- c()
+    if(input$data_visualize == "Hosp.Rec"){
+      for( i in 1:(length(data[,1])) ){choices<-c(choices,(paste("record ",i)))}
+      radioButtons("patient", label="Choose a patient record",choices=choices)
+    } else if (input$data_visualize == "feat"){
+      for( i in 1:(length(data[1,])) ){choices<-c(choices,(colnames(data)[i]=i))}
+      radioButtons("feature", label="Choose a feature",choices=choices)
+    }
+    
   })
   
-
+  output$data_stat <- renderPlot({
+    if( input$data_visualize == "feat" ){
+      #plot( data[,c("age")] )
+      #plot( data[,strtoi(input$feature)] )
+      plot( data[,18] )
+      #input$feature
+    }
+  })
   
 })
--- ui.R
+++ ui.R
@@ -18,7 +18,25 @@ shinyUI(
     collapsible = TRUE,
     theme = shinytheme("cerulean"),
     tabPanel(strong("Home")),
-    tabPanel(strong("Data")),
+    tabPanel(
+      strong("Data"),
+      fluidRow(
+        column(4,
+          fileInput("data_file", "Choose CSV File",
+                    accept = c("text/csv","text/comma-separated-values,text/plain",".csv")
+          ),
+          tags$hr(),
+          checkboxInput('header', 'Header', TRUE),
+          radioButtons('sep', 'Separator',c(Comma=',',Semicolon=';',Tab='\t'),','),
+          selectInput("data_visualize",NULL,
+                      c("Features" = "feat","Hospitaliuzation Records" = "Hosp.Rec")),
+          tableOutput("data_contents")
+        ),
+        column(8,
+          plotOutput("data_stat")   
+        )
+      )
+    ),
     tabPanel(strong("Prediction")),
     tabPanel(strong("Classification"))
   )
