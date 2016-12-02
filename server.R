9409fbf965ba939269945cf9e4688ea6171a0d1c
MrAurelienB <aurelien.bach@hotmail.fr>
2016-12-02 15:16:21 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -9,60 +9,108 @@
 
 library(shiny)
 library(ggplot2)
+library(survival)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   code_feat <- list()
+  copd <- NULL
   
-  output$data_contents <- renderUI({
-    inFile <<- input$data_file
-    if (is.null(inFile))
-      return(NULL)
-    
-    data <<- read.csv(inFile$datapath, header = input$header, sep = ',')
-    if( length(data[1,]) == 1 )
-      data <<- read.csv(inFile$datapath, header = input$header, sep = ';')
-    if( length(data[1,]) == 1 )
-      data <<- read.csv(inFile$datapath, header = input$header, sep = '\t')
-    
-    code_record <- as.list(1:(length(data[,1])))
-    names(code_record) <- paste(as.list(rep("Record",length(data[1,]))),as.character(code_record))
-    code_feat <<- as.list(1:(length(data[1,])))
-    names(code_feat) <<- as.list(colnames(data))
-    
-    if(input$data_visualize == "Hosp.Rec"){
-      radioButtons("patient", label="Choose a patient record",choices=code_record)
-    } else if (input$data_visualize == "feat"){
-      radioButtons("feature", label="Choose a feature",choices=code_feat)
+  observeEvent({
+    input$data_file
+    input$patientID},
+    {
+  
+    readFile <- function(){
+      copd <<- read.csv(inFile$datapath, header = input$header, sep = ',')
+      if( length(copd[1,]) == 1 )
+        copd <<- read.csv(inFile$datapath, header = input$header, sep = ';')
+      if( length(copd[1,]) == 1 )
+        copd <<- read.csv(inFile$datapath, header = input$header, sep = '\t')
+      
+      copd
     }
+    
+    output$data_contents <- renderUI({
+      inFile <<- input$data_file
+      if (is.null(inFile))
+        return(NULL)
+      
+      copd <<- readFile()
+      
+      code_record <- as.list(1:(length(copd[,1])))
+      if( input$patientID ){
+        names(code_record) <- as.character(copd[,1])
+        copd <<- copd[,-c(1)]
+      }else{
+        names(code_record) <- paste(as.list(rep("Record",length(copd[1,]))),as.character(code_record))
+        copd <<- readFile()
+      }
+      code_feat <<- as.list(1:(length(copd[1,])))
+      names(code_feat) <<- as.list(colnames(copd))
+      
+      if(input$data_visualize == "Hosp.Rec"){
+        selectInput("patient",NULL,choices=code_record)
+      } else if (input$data_visualize == "feat"){
+        selectInput("feature",NULL,choices=code_feat)
+      }
+    })
   })
   
-  observeEvent(input$data_file, {
-    output$data_stat <- renderPlot({
+  observeEvent({
+    input$data_file
+    input$patientID
+    },
+    {output$data_stat <- renderPlot({
       index_feat <- strtoi(input$feature)
       index_event <- strtoi(input$event)
       if( input$data_visualize == "feat" ){
-        if( length(unique(data[,index_feat])) > 5 ){
-          #hist(data[,index_feat],col="lightblue",xlab=colnames(data)[index_feat],main=paste("Histogram of ",colnames(data)[index_feat]))
-          m <- ggplot(data, aes(x=data[,index_feat]))+geom_histogram(aes(fill=..count..))
-          m <- m + labs(x=colnames(data)[index_feat]) + labs(title=paste("Histogram of",colnames(data)[index_feat]))
+        if( length(unique(copd[,index_feat])) > 5 ){
+          m <- ggplot(copd, aes(x=copd[,index_feat]))+geom_histogram(aes(fill=..count..))
+          m <- m + labs(x=colnames(copd)[index_feat]) + labs(title=paste("Histogram of",colnames(copd)[index_feat]))
           m + scale_fill_gradient("Count",low="blue4",high="lightblue")      
         }else{
-          #barplot( table( data[,index_feat] ),col="lightblue",xlab=colnames(data[,strtoi(input$feature)]),ylab=colnames(data)[,strtoi(input$event)])
-          #plot( factor(data[,index_feat]) ~ factor(data[,index_event]),col=c("cadetblue2","lightblue"),xlab=colnames(data)[index_feat],ylab=colnames(data)[index_event],main="")
-          m <- qplot(factor(data[,index_event]), data=data, geom="bar", fill=factor(data[,index_feat]))
-          m <- m + labs(title=paste("Count of '",colnames(data)[index_feat],"' depending on '",colnames(data)[index_event],"'"))
-          m <- m + labs(x=colnames(data)[index_event]) + labs(y="count") + labs(fill=colnames(data)[index_feat])
+          m <- qplot(as.factor(copd[,index_event]), data=copd, geom="bar", fill=as.factor(copd[,index_feat]))
+          m <- m + labs(title=paste("Count of '",colnames(copd)[index_feat],"' depending on '",colnames(copd)[index_event],"'"))
+          m <- m + labs(x=colnames(copd)[index_event]) + labs(y="count") + labs(fill=colnames(copd)[index_feat])
           m
         }
       }
     })
   })
   
-  observeEvent(input$data_file, {
-    output$data_event_choice <- renderUI({
-      selectInput("event",NULL,code_feat)
+  observeEvent({
+    input$data_file
+    input$patientID
+    },
+    {output$data_event_choice <- renderUI({
+      selectInput("event",NULL,choices=code_feat,selected=length(code_feat)-1)
+    })
+  })
+  
+  observeEvent({
+    input$data_file
+    input$patientID
+    },
+    {output$data_stime_choice <- renderUI({
+      selectInput("surv_time",NULL,choices=code_feat,selected=length(code_feat))
+    })
+  })
+  
+  observeEvent({
+    input$data_file
+    input$patientID
+    },
+    {output$survival_curve <- renderPlot({
+      index_time <- strtoi(input$surv_time)
+      index_feat <- strtoi(input$feature)
+      index_event <- strtoi(input$event)
+      if( input$data_visualize == "feat" && length(unique(copd[,index_feat])) < 5 ){
+        surv_obj <- Surv(copd[,index_time],copd[,index_event]==1)
+        fit <- survfit(surv_obj~copd[,index_feat] , data = copd )
+        ggsurvplot(fit,risk.table=TRUE,break.time.by=ceiling(max(copd[,index_time])/5),risk.table.y.text=FALSE,risk.table.height=0.35)
+      }
     })
   })
   
--- ui.R
+++ ui.R
@@ -19,6 +19,10 @@ shinyUI(
     theme = shinytheme("cerulean"),
     tabPanel(
       strong("Home"),
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"
+      ),
       fluidRow(
         column(12,
                h1("Web Application for COPD prediction", align = "center"),
@@ -35,37 +39,61 @@ shinyUI(
     ),
     tabPanel(
       strong("Data"),
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"
+      ),
       fluidRow(
         column(4,
           fileInput("data_file", "Choose CSV File",
                     accept = c("text/csv","text/comma-separated-values,text/plain",".csv")
           ),
           checkboxInput('header', 'Header', TRUE),
+          checkboxInput('patientID', 'Patient ID', FALSE),
           tags$hr(),
           #radioButtons('sep', 'Separator',c(Comma=',',Semicolon=';',Tab='\t'),','),
           wellPanel("Event of Interest",
                     tableOutput("data_event_choice")
           ),
+          wellPanel("Survival Time",
+                    tableOutput("data_stime_choice")
+          ),
           selectInput("data_visualize",NULL,
                       c("Features" = "feat","Hospitaliuzation Records" = "Hosp.Rec"),
                       selected="feat"),
           wellPanel("Select one object",
-                    style = "overflow-y:scroll; max-height: 400px; float:left",
                     tableOutput("data_contents")
           )
         ),
         column(8,
-          plotOutput("data_stat")   
+          plotOutput("data_stat"),
+          plotOutput("survival_curve")
         )
       )
     ),
-    tabPanel(strong("Prediction")),
+    tabPanel(
+      strong("Prediction"),
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"
+      ),
+      fluidRow(
+        column(4,
+            wellPanel("Select a Model",
+              selectInput("model",NULL,
+                          c("Cox Model"="coxmodel","Our Model"="ourmodel"),
+                          selected="coxmodel")
+            ),
+            wellPanel("k-fold Cross-Validation",
+              selectInput("k-folds",NULL,1:5)
+            )
+        ),
+        column(8)
+      )
+    ),
     tabPanel(strong("Classification"))
   )
 )
-
-
-
 
 
 
